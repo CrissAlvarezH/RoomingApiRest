0eea68d4e9e843821a6d79367d413b623267f608
Cristian <alvarezhernandezcristian1@gmail.com>
2017-08-21 16:48:38 -0500

    Agrego constantes para los INSERT de los modelos
    Las constantes CREACION_EXITOSA y CREACION_FALLIDA
    fueron creadas para ser usadas a la hora de retornar
    una bandera que indique si un INSER INTO de alguna
    entidad fue exitoso

--- utilidades/constantes.php
+++ utilidades/constantes.php
@@ -3,6 +3,10 @@
 const METODO_NO_SOPORTADO = 500;
 const RECURSO_CREADO = 100;
 const RECURSO_NO_ENCONTRADO = 200;
-const PDO_ERROR = 300;
+
+const PDO_ERROR = 300;// error en la conexion en PDO
+
+const CREACION_EXITOSA = 1;// cuando se hace un INSER exitoso
+const CREACION_FALLIDA = 2;// ||     ||   ||    ||   fallido
 
 ?>
