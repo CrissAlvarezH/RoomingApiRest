e877cb076493664c733e5f2341a02c6304455c89
Cristian <alvarezhernandezcristian1@gmail.com>
2017-08-21 20:03:17 -0500

    Creo una interfaz para usar de base en Controlador
    Creo la interfaz BaseControlador la cual lleva dentro
    los metodos get, post, put y delete, los cuales seran
    los metodos que llecaran los controladores, por el momento
    no se implementara esta interfaz ya que no se usaran todos los
    metodo ahi presentes

---
+++ controladores/baseControlador.php
@@ -0,0 +1,29 @@
+<?php
+
+/** POR EL MOMENTO NO SE USARÁ, YA QUE NO SE IMPLEMENTARAN TODOS LO METODOS
+ * Define el coomportamiento de los controladores
+ */
+interface BaseControlador {
+	/** Metodo encargado de atender la peticion hecha por GET
+	* @param $peticion es la peticion como 1 ó 1/grupos ó materias/4, etc...
+	*/
+	public function get($peticion);
+
+	/** Metodo encargado de atender la peticion hecha por POST
+	* @param $peticion es la peticion como 'login' ó 'registro'
+	*/
+	public function post($peticion);
+
+	/** Metodo encargado de atender la peticion hecha por PUT
+	* @param $peticion trae el codigo de el recurso que se desea modificar
+	*/
+	public function put($peticion);
+
+	/** Metodo encargado de atender la peticion hecha por DELETE
+	* @param $peticion trae el codigo de el recurso que se desea borrar
+	*/
+	public function delete($peticion);
+}
+
+
+?>
