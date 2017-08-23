755a635aed19c29dc88213f133a0856c39ff9b4d
Cristian <alvarezhernandezcristian1@gmail.com>
2017-08-23 11:25:06 -0500

    Agrego metodos obtenedores en AlumnosModelo
    Agrego getTodos() para obtener todos los alumnos,
    getPorId(idAlumno) para obtener un alumno con el id
    pasado por parametros, getGrupos(id) el cual retorna
    los grupos que estan relacionados con el alumno con el
    id pasado por parametros

--- modelos/alumnosModelo.php
+++ modelos/alumnosModelo.php
@@ -1,5 +1,7 @@
 <?php
 require 'datos/conexionBD.php';
+require 'utilidades/constantes.php';
+require 'urilidades/exceptionApi.php';
 
 class AlumnosModelo {
 	private const NOMBRE_TABLA = "alumnos";
@@ -9,28 +11,105 @@ class AlumnosModelo {
 	private const CONTRASENA = "contrasena_alumnos";
 
 	public static function insertar($datosAlumno){
-		$conexion = Conexion::getInstancia()->getConexion();
+		try{
+			$conexion = Conexion::getInstancia()->getConexion();
 
-		$query = "INSERT INTO ".NOMBRE_TABLA." ( ".ID.", ".NOMBRE.", ".APELLIDOS
-			.", ".CONTRASENA.") VALUES (?, ?, ?, ?);";
+			$query = "INSERT INTO ".NOMBRE_TABLA." ( ".ID.", ".NOMBRE.", ".APELLIDOS
+				.", ".CONTRASENA.") VALUES (?, ?, ?, ?);";
 
-		$sentencia = $conexion->prepare($query);
+			$sentencia = $conexion->prepare($query);
 
-		$sentencia->bindParam(1, $datosAlumno->id);
-		$sentencia->bindParam(2, $datosAlumno->nombre);
-		$sentencia->bindParam(3, $datosAlumno->apellidos);
-		$sentencia->bindParam(4, $datosAlumno->contrasena);
+			$sentencia->bindParam(1, $datosAlumno->id);
+			$sentencia->bindParam(2, $datosAlumno->nombre);
+			$sentencia->bindParam(3, $datosAlumno->apellidos);
+			$sentencia->bindParam(4, $datosAlumno->contrasena);
 
-		if($sentencia->execute()){
-			[
-				"estado" => CREACION_EXITOSA,
-				"mensaje" => "alumno creado"
-			];
-		}else{
-			throw new ExceptionApi(CREACION_FALLIDA, "error en la sentencia");
+			if($sentencia->execute()){
+				return
+					[
+						"estado" => CREACION_EXITOSA,
+						"mensaje" => "alumno creado"
+					];
+			}else{
+				throw new ExceptionApi(CREACION_FALLIDA, "error en la sentencia");
+			}
+		}catch(PDOException $e){
+			throw new ExceptionApi(PDO_ERROR, "error en conexion PDO");
 		}
-	}catch(PDOException $e){
-		throw new ExceptionApi(PDO_ERROR, "error en conexion PDO");
+	}
+
+	public static function getTodos(){
+		try{
+			$conexion = Conexion::getInstancia()->getConexion();
+
+			$sentencia = $conexion->prepare("SELECT * FROM ".NOMBRE_TABLA.";");
+
+			if($sentencia->execute()){
+				http_response_code(200);
+				return
+					[
+						"estado" => ESTADO_EXITOSO,
+						"datos" => $sentencia->fetchAll(PDO::FETCH_ASSOC)
+					];
+			}else{
+				throw new ExceptionApi(ESTADO_FALLIDO, "error en la consulta");
+			}
+		}catch(PDOException $e){
+			throw new ExceptionApi(PDO_ERROR, "error en conexion PDO");
+		}
+	}
+
+	public static function getPorId($id){
+		try{
+			$conexion = Conexion::getInstancia()->getConexion();
+
+			$query = "SELECT * FROM ".NOMBRE_TABLA." WHERE id_alumno = ?;";
+
+			$sentencia = $conexion->prepare($query);
+
+			$sentencia->bindParam(1, $id);
+
+			if($sentenacia->execute()){
+				http_response_code(200);
+				return
+					[
+						"estado" => ESTADO_EXITOSO,
+						"datos" => $sentencia->fetchAll(PDO::FETCH_ASSOC)
+					];
+			}else{
+				throw new ExceptionApi(ESTADO_FALLIDO, "error en la consulta");
+			}
+		}catch(PDOException $e){
+			throw new ExceptionApi(PDO_ERROR, "error en conexion PDO");
+		}
+	}
+
+	public static function getGrupos($idAlumno){
+		try{
+			$conexion = Conexion::getInstancia()->getConexion();
+
+			$query = "SELECT numero_grupo, nombre_materia, creditos_materia "
+				."FROM grupos, alumno_grupo, materias "
+				."WHERE numero_grupo = numero_grupo_alumno "
+				."AND cod_materia = cod_materia_grupo AND id_alumno_grupo = ?;";
+
+			$sentencia = $conexion->prepare($query);
+
+			$sentencia->bindParam(1, $idAlumno);
+
+			if($sentencia->execute()){
+				http_response_code(200);
+				return
+					[
+						"estado" => ESTADO_EXITOSO.
+						"datos" => $sentencia->fetchAll(PDO::FETCH_ASSOC)
+					];
+			}else{
+				throw new ExceptionApi(ESTADO_FALLIDO, "error en la consulta");
+			}
+		}catch(PDOException $e){
+			throw new ExceptionApi(PDO_ERROR, "error en conexion PDO");
+		}
 	}
 }
 
